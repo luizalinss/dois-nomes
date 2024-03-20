826414009ed7cbc58eb80a9b5fe4a8dc3ef8f65b
luizalinss <160612268+luizalinss@users.noreply.github.com>
2024-03-20 10:03:09 -0300

    Add files via upload

---
+++ lulu.py
@@ -0,0 +1,40 @@
+print ("luiza")
+
+número = 10
+nome = "luiza"
+booleano = True 
+a = 10
+b = 5
+soma = a + b
+subtração = a - b
+multiplicação = a * b
+divisão = a /  b
+c = 5
+d = 6
+e = 7
+f = 8
+frase = "eu sou tude"
+palavra = "louca" 
+média = (a + b + c + d) / 4
+print("A média é", média)
+
+quadrado = c * c
+print ("O quadrado do número 5 é", quadrado)
+
+dobro = c * 2
+print (" O dobro do número 5 é", dobro)
+
+frase = "eu sou tude"
+quantidade_letras = len (frase)
+print ("A frase tem", quantidade_letras, "letras.")
+
+quantidade_espaços = frase.count (" ")
+print (" a quantidade de espaços na frase é", quantidade_espaços)
+
+if c > d:
+    print ("o c é maior que d.")
+else:
+    print ("o d é maior que a.")
+
+maior_número = max (c,d,e,f)
+print ("o maior número é:", maior_número)
