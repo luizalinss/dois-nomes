83ad7b72ed3f17a2e925ab03370d24aa9854b9bf
luizalinss <160612268+luizalinss@users.noreply.github.com>
2024-03-27 10:51:19 -0300

    Add files via upload

--- lulu.py
+++ lulu.py
@@ -1,40 +1,39 @@
-print ("luiza")
+def soma(a,b):
+    return a+b 
+def quadrado (a):
+    return a**2
+def hipotenusa (catl, cat2):
+    return soma (quadrado(catl), quadrado(cat2))**(1/2)
+def simples (cor):
+    if cor == 'azul':
+        return 'escolheu certo'
+def medio (cor):
+    if cor == 'azul':
+        return 'escolheu certo'
+    else:
+        return 'tente outra cor'
+def completo (cor):
+    if cor == 'azul':
+        return 'escolheu certo'
+    elif cor == 'marrom':
+        return 'não tem salvação'
+    else:
+        return 'tente outra cor'
 
-número = 10
-nome = "luiza"
-booleano = True 
-a = 10
-b = 5
-soma = a + b
-subtração = a - b
-multiplicação = a * b
-divisão = a /  b
-c = 5
-d = 6
-e = 7
-f = 8
-frase = "eu sou tude"
-palavra = "louca" 
-média = (a + b + c + d) / 4
-print("A média é", média)
-
-quadrado = c * c
-print ("O quadrado do número 5 é", quadrado)
-
-dobro = c * 2
-print (" O dobro do número 5 é", dobro)
-
-frase = "eu sou tude"
-quantidade_letras = len (frase)
-print ("A frase tem", quantidade_letras, "letras.")
-
-quantidade_espaços = frase.count (" ")
-print (" a quantidade de espaços na frase é", quantidade_espaços)
-
-if c > d:
-    print ("o c é maior que d.")
-else:
-    print ("o d é maior que a.")
-
-maior_número = max (c,d,e,f)
-print ("o maior número é:", maior_número)
+numeros = [1, 2, 3, 4, 5]
+print(numeros[0])
+print(numeros[-1])
+numeros[0] = 10
+print(numeros)
+contador = 0
+while contador < 10:
+    print(contador)
+    contador += 1
+    for i in range(10):
+        print(i)
+    for item in [1,45,78,'a',(3,5)]:
+        print(item)
+    for letra in 'minha string':
+        print(letra)
+      
+    
